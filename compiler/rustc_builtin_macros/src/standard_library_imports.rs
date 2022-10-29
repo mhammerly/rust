df8d3b7af553e7905121921115b458ea4275daf4
Matt Hammerly <hammerly.matt@gmail.com>
2022-10-28 20:35:55 -0700

    ignore #![no_std] and #![no_core] when they are passed in with --extern

--- compiler/rustc_builtin_macros/src/standard_library_imports.rs
+++ compiler/rustc_builtin_macros/src/standard_library_imports.rs
@@ -8,6 +8,11 @@ use rustc_span::symbol::{kw, sym, Ident, Symbol};
 use rustc_span::DUMMY_SP;
 use thin_vec::thin_vec;
 
+struct SysrootExterns {
+    std: bool,
+    core: bool,
+}
+
 pub fn inject(
     mut krate: ast::Crate,
     resolver: &mut dyn ResolverExpand,
@@ -15,17 +20,41 @@ pub fn inject(
 ) -> ast::Crate {
     let edition = sess.parse_sess.edition;
 
+    // We want to ignore `#![no_std]` and `#![no_core]` if the corresponding crate has
+    // explicitly been passed to `--extern`.
+    let sysroot_externs: SysrootExterns =
+        sess.opts.externs.iter().filter(|(_, entry)| entry.add_prelude).fold(
+            SysrootExterns { std: false, core: false },
+            |acc, (name, _)| SysrootExterns {
+                std: acc.std || Ident::from_str(name).name == sym::std,
+                core: acc.core || Ident::from_str(name).name == sym::core,
+            },
+        );
+
+    // FIXME: rustc_std_workspace_core apparently passes core via extern so this is false.
+    // It goes on to the `no_std` branch. Because it is not compiler_builtins,
+    // compiler_builtins is made an AST item. However, compiler_builtins is not
+    // passed on the command line, so the build errors with it missing.
+    // let no_core = sess.contains_name(&krate.attrs, sym::no_core) && !sysroot_externs.core;
+    let no_core = sess.contains_name(&krate.attrs, sym::no_core);
+
+    // FIXME: no_core should probably imply no_std
+    let no_std = sess.contains_name(&krate.attrs, sym::no_std) && !sysroot_externs.std;
+
+    let compiler_rt = sess.contains_name(&krate.attrs, sym::compiler_builtins);
+
     // the first name in this list is the crate name of the crate with the prelude
-    let names: &[Symbol] = if sess.contains_name(&krate.attrs, sym::no_core) {
+    let names: &[Symbol] = if no_core {
         return krate;
-    } else if sess.contains_name(&krate.attrs, sym::no_std) {
-        if sess.contains_name(&krate.attrs, sym::compiler_builtins) {
-            &[sym::core]
-        } else {
-            &[sym::core, sym::compiler_builtins]
-        }
+    } else if no_std {
+        if compiler_rt { &[sym::core] } else { &[sym::core, sym::compiler_builtins] }
     } else {
-        &[sym::std]
+        if sysroot_externs.core {
+            // FIXME: Document why this was necessary
+            &[sym::std, sym::core]
+        } else {
+            &[sym::std]
+        }
     };
 
     let expn_id = resolver.expansion_for_ast_pass(
@@ -47,15 +76,14 @@ pub fn inject(
         } else {
             Ident::new(name, call_site)
         };
-        krate.items.insert(
-            0,
-            cx.item(
-                span,
-                ident,
-                thin_vec![cx.attribute(cx.meta_word(span, sym::macro_use))],
-                ast::ItemKind::ExternCrate(None),
-            ),
-        );
+        // If `std` is present, we don't want `#[macro_use]` for `core` but we still want
+        // the item.
+        let attrs = if ident.name != sym::core || !sysroot_externs.std {
+            thin_vec![cx.attribute(cx.meta_word(span, sym::macro_use))]
+        } else {
+            thin_vec![]
+        };
+        krate.items.insert(0, cx.item(span, ident, attrs, ast::ItemKind::ExternCrate(None)));
     }
 
     // The crates have been injected, the assumption is that the first one is
